0154f5d2771fc201e3689668166354632e83b52f
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-21 20:35:57 -0300

    comentario alter

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ btn1.addEventListener('click',()=>{
     inputUsuario.value = ''
 })
 /*
-Função que é executada quando o botão de favorito é clicado
+Função 40222
 */
 
 const favoritoClicado = (eventoDeClique) => {
